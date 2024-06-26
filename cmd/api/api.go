6db1d440055e682f44d36aa891ecd1dec6271992
Rendayse <d1ma.jashkov@yandex.ru>
2024-06-26 12:13:30 +0300

    server setup is completes

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -2,6 +2,7 @@ package api
 
 import (
 	"database/sql"
+	"gihub.com/Yahar4/service/user"
 	"github.com/gorilla/mux"
 	"log"
 	"net/http"
@@ -22,6 +23,9 @@ func NewAPIServer(addr string, db *sql.DB) *APIServer {
 func (s *APIServer) Run() error {
 	router := mux.NewRouter()
 	subrouter := router.PathPrefix("/api/v1").Subrouter()
+
+	userHandler := user.NewHandler()
+	userHandler.RegisterRoutes(subrouter)
 
 	log.Println("listening on address ", s.Addr)
 
